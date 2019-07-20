ac8de277386d19888e022fd929a00a525cc12e92
rahulmittal <mittal_ims@yahoo.com>
2019-07-20 19:39:55 +0530

    get users from pool

--- SendSMS/lambda.js
+++ SendSMS/lambda.js
@@ -1,4 +1,6 @@
+
 let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
@@ -6,7 +8,28 @@ exports.handler = function (event, context, callback) {
     let sender = event['sender'];
     let message = event['message'];
     let isPromotional = true;
-    console.log("Sending message{", message, "}to receiver{", receiver+"}");
+    console.log("Sending message{", message, "}to receiver{", receiver + "}");
+
+    /**
+     * Get the list of users and check if the mobile no exists in the list.
+     */
+    cognito_idp.listUsers({
+        UserPoolId: "us-east-1_ZYvkePp7C",
+        AttributesToGet: [
+            "sub",
+            "name",
+            "email",
+            "phone_number"
+        ],
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            console.log("cognito_idp.listUsers - failed", err);
+            throw error;
+        }
+        console.log("list of users.");console.log(data);
+    });
+
     sns.publish({
         Message: message,
         MessageAttributes: {
@@ -21,17 +44,17 @@ exports.handler = function (event, context, callback) {
             'AWS.SNS.SMS.MaxPrice': {
                 'DataType': 'Number',
                 'NumberValue': 0.50
-            }  */          
+            }  */
         },
         PhoneNumber: receiver
     }).promise()
         .then(data => {
             console.log("Sent message to", receiver + " response data :"); console.log(data);
-			callback(null, data);
+            callback(null, data);
         })
         .catch(err => {
             console.log("Sending failed", err);
-			callback(err);
+            callback(err);
         });
 
     callback(null, 'Successfully executed');
