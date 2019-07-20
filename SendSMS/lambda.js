b7f956b967c5f34bac2a8458f933615d52bc5078
rahulmittal <mittal_ims@yahoo.com>
2019-07-20 13:03:26 +0530

    added trans

--- SendSMS/lambda.js
+++ SendSMS/lambda.js
@@ -6,23 +6,27 @@ exports.handler = function (event, context, callback) {
     let sender = event['sender'];
     let message = event['message'];
     let isPromotional = true;
-    console.log("Sending message", message, "to receiver", receiver);
+    console.log("Sending message{", message, "}to receiver{", receiver+"}");
     sns.publish({
         Message: message,
         MessageAttributes: {
             'AWS.SNS.SMS.SMSType': {
                 'DataType': 'String',
-                'StringValue': 'Promotional'
+                'StringValue': 'Transactional'
             },
             'AWS.SNS.SMS.SenderID': {
                 'DataType': 'String',
                 'StringValue': sender
-            }
+            }/*,
+            'AWS.SNS.SMS.MaxPrice': {
+                'DataType': 'Number',
+                'NumberValue': 0.50
+            }  */          
         },
         PhoneNumber: receiver
     }).promise()
         .then(data => {
-            console.log("Sent message to", receiver);
+            console.log("Sent message to", receiver + " response data :"); console.log(data);
 			callback(null, data);
         })
         .catch(err => {
